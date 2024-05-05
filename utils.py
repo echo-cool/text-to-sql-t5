79795d6c5e3e7fa6b4c79c909e6dba6cdd4024f7
Yuyang Wang <yw2545@cornell.edu>
2024-05-04 23:25:46 -0400

    update-2024-05-04

--- utils.py
+++ utils.py
@@ -106,7 +106,7 @@ def compute_records(processed_qs: List[str]):
         * processed_qs (List[str]): The list of SQL queries to execute
     """
     num_threads = 30
-    timeout_secs = 120
+    timeout_secs = 60
 
     pool = ThreadPoolExecutor(num_threads)
     futures = []
